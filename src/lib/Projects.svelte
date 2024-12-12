8151a03f2de1d0ae3049c9707cf0a3bbba2b7f83
crvd <casparvandam@hotmail.com>
2024-12-12 23:56:32 +0100

    streamlined card styling

--- src/lib/Projects.svelte
+++ src/lib/Projects.svelte
@@ -5,16 +5,18 @@
 
 <div class="projects">
   <h1 class="projects__header">Projects</h1>
-  {#each projects as project}
-    <ProjectCard
-      title={project.title}
-      imageName={project.imageName}
-      text={project.text}
-      github={project.github}
-      web={project.web}
-      tech={project.tech}
-    />
-  {/each}
+  <div class="projects-wrapper">
+    {#each projects as project}
+      <ProjectCard
+        title={project.title}
+        imageName={project.imageName}
+        text={project.text}
+        github={project.github}
+        web={project.web}
+        tech={project.tech}
+      />
+    {/each}
+  </div>
 </div>
 
 <style>
@@ -29,5 +31,11 @@
 
   .projects__header {
     margin-bottom: 2rem;
+  }
+
+  @media (min-width: 768px) {
+    .projects-wrapper {
+      display: flex;
+    }
   }
 </style>
